a334bf0847b8b09e399992536e5f142dd0afbc09
Orel Adivi <57455071+orel-adivi@users.noreply.github.com>
2021-08-15 20:05:59 +0300

    adding test cases

--- src/test/java/il/org/spartan/protolog/ParserTest.java
+++ src/test/java/il/org/spartan/protolog/ParserTest.java
@@ -4,17 +4,27 @@
  */
 package il.org.spartan.protolog;
 
-import il.org.spartan.*;
-import org.junit.*;
 import org.junit.jupiter.api.Test;
 
 public class ParserTest {
+
+
+  /* ---------------------------------------- Testing General Cases: ---------------------------------------- */
 
   /**
    * This test checks that that static parsing function (Parser.parse()) exists.
    */
   @Test public void testParserExists() {
     Parser.parse("a");
+  }
+
+  /**
+   * This test checks that that static parsing function creates a new term each time.
+   */
+  @Test public void testCloning() {
+    Term result1 = Parser.parse("a");
+    Term result2 = Parser.parse("a");
+    assert(result1 != result2);
   }
 
   /**
@@ -33,47 +43,109 @@ public class ParserTest {
     assert(result == null);
   }
 
+
+  /* ---------------------------------------- Testing String Literals: ---------------------------------------- */
+
   /**
-   * This test checks that the parser parses correctly one-letter constants.
+   * This test checks that the parser parses correctly one-letter constants (checking all options).
    */
   @Test public void testShortConstants() {
-    //todo
+    for(char letter = 'a'; letter <= 'z'; letter++) {
+      Term result = Parser.parse(Character.toString(letter));
+      assert(result instanceof Term.Atom.Constant);
+      assert((new Term.Atom.Constant(Character.toString(letter))).equals(result));
+    }
   }
 
   /**
    * This test checks that the parser parses correctly multiple-letter constants.
    */
   @Test public void testLongConstants() {
-    //todo
+    String[] cases = new String[]{"hi", "test", "divide", "pArEnT", "rELATION"};
+    for(String str : cases) {
+      Term result = Parser.parse(str);
+      assert(result instanceof Term.Atom.Constant);
+      assert((new Term.Atom.Constant(str)).equals(result));
+    }
   }
 
+
+  /* ---------------------------------------- Testing Number Literals: ---------------------------------------- */
+
   /**
-   * This test checks that the parser parses correctly one-letter numbers.
+   * This test checks that the parser parses correctly up to three-letter positive numbers (checking all options).
    */
-  @Test public void testShortNumbers() {
-    //todo
+  @Test public void testShortPositiveNumbers() {
+    for(int num = 0; num <= 999; num++) {
+      Term result = Parser.parse(Integer.toString(num));
+      assert(result instanceof Term.Atom.Num);
+      assert((new Term.Atom.Num(num)).equals(result));
+    }
   }
 
   /**
-   * This test checks that the parser parses correctly multiple-letter numbers.
+   * This test checks that the parser parses correctly multiple-letter positive numbers.
    */
-  @Test public void testLongNumbers() {
-    //todo
+  @Test public void testLongPositiveNumbers() {
+    int[] cases = new int[]{1000, 100001, Integer.MAX_VALUE};
+    for(int num : cases) {
+      Term result = Parser.parse(Integer.toString(num));
+      assert(result instanceof Term.Atom.Num);
+      assert((new Term.Atom.Num(num)).equals(result));
+    }
   }
 
   /**
-   * This test checks that the parser parses correctly one-letter variables.
+   * This test checks that the parser parses correctly up to three-letter negative numbers (checking all options).
+   */
+  @Test public void testShortNegativeNumbers() {
+    for(int num = -1; num >= -999; num--) {
+      Term result = Parser.parse(Integer.toString(num));
+      assert(result instanceof Term.Atom.Num);
+      assert((new Term.Atom.Num(num)).equals(result));
+    }
+  }
+
+  /**
+   * This test checks that the parser parses correctly multiple-letter negative numbers.
+   */
+  @Test public void testLongNegativeNumbers() {
+    int[] cases = new int[]{-1000, -100001, Integer.MIN_VALUE};
+    for(int num : cases) {
+      Term result = Parser.parse(Integer.toString(num));
+      assert(result instanceof Term.Atom.Num);
+      assert((new Term.Atom.Num(num)).equals(result));
+    }
+  }
+
+
+  /* ---------------------------------------- Testing Variables: ---------------------------------------- */
+
+  /**
+   * This test checks that the parser parses correctly one-letter variables (checking all options).
    */
   @Test public void testShortVariables() {
-    //todo
+    for(char letter = 'A'; letter <= 'Z'; letter++) {
+      Term result = Parser.parse(Character.toString(letter));
+      assert(result instanceof Term.Atom.Variable);
+      assert((new Term.Atom.Variable(Character.toString(letter))).equals(result));
+    }
   }
 
   /**
    * This test checks that the parser parses correctly multiple-letter variables.
    */
   @Test public void testLongVariables() {
-    //todo
+    String[] cases = new String[]{"Result", "TEST", "NuMbEr"};
+    for(String str : cases) {
+      Term result = Parser.parse(str);
+      assert(result instanceof Term.Atom.Variable);
+      assert((new Term.Atom.Variable(str)).equals(result));
+    }
   }
+
+
+  /* ---------------------------------------- Testing Compound Terms: ---------------------------------------- */
 
   /**
    * This test checks that the parser parses correctly compounds whose children are all non-compound terms.
