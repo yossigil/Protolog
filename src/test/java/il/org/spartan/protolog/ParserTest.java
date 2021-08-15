8f0340995185f101f0a0201cab92359cc702becd
Orel Adivi <57455071+orel-adivi@users.noreply.github.com>
2021-08-15 20:10:18 +0300

    spacing

--- src/test/java/il/org/spartan/protolog/ParserTest.java
+++ src/test/java/il/org/spartan/protolog/ParserTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 
 public class ParserTest {
 
-
   /* ---------------------------------------- Testing General Cases: ---------------------------------------- */
 
   /**
