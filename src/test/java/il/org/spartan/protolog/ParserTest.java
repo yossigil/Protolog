016099a77186b584c38a90ab171c573279b20940
Orel Adivi <57455071+orel-adivi@users.noreply.github.com>
2021-08-15 21:09:08 +0300

    writing tests

--- src/test/java/il/org/spartan/protolog/ParserTest.java
+++ src/test/java/il/org/spartan/protolog/ParserTest.java
@@ -7,7 +7,26 @@ package il.org.spartan.protolog;
 import org.junit.jupiter.api.Test;
 
 /**
- * todo
+ * ParserTest - this class contains 17 test cases for Parse class.
+ *
+ * The following things are checked:
+ *  - existence of static parsing function
+ *  - the parsing function creates a new term each time
+ *  - returning null on null-string
+ *  - returning null on empty-string
+ *  - returning correct value for all one-letter constants
+ *  - returning correct value for multiple-letter constants
+ *  - returning correct value for the number zero
+ *  - returning correct value for all positive numbers up to three-letter
+ *  - returning correct value for multiple-letter positive numbers
+ *  - returning correct value for all negative numbers up to three-letter
+ *  - returning correct value for multiple-letter negative numbers
+ *  - returning correct value for all one-letter variables
+ *  - returning correct value for multiple-letter variables
+ *  - returning correct value for compounds whose children are all non-compound terms
+ *  - returning correct value for compounds whose children are all compounds
+ *  - returning correct value for  compounds whose children are compounds and non-compound terms
+ *  - processing spaces correctly for compounds
  */
 public class ParserTest {
 
@@ -239,6 +258,64 @@ public class ParserTest {
      */
     @Test
     public void testMixedCompounds() {
-        //todo
+
+        // checking "equal(X, test(a))" :
+        final String string1 = "equal(X, test(a))";
+        final Term term1 = new Term.Compound("equal",
+                new Term.Atom.Variable("X"),
+                new Term.Compound("test", new Term.Atom.Constant("a")));
+        final Term result1 = Parser.parse(string1);
+        assert (result1 instanceof Term.Compound);
+        assert (term1.equals(result1));
+
+        // checking "equal(testing(X, inverse(0, Y)),testing(1, Y))" :
+        final String string2 = "equal(testing(X, inverse(0, Y)),testing(1, Y))";
+        final Term term2 = new Term.Compound("equal",
+                new Term.Compound("testing",
+                        new Term.Atom.Variable("X"),
+                        new Term.Compound("inverse",
+                                new Term.Atom.Num(0),
+                                new Term.Atom.Variable("Y"))),
+                new Term.Compound("testing",
+                        new Term.Atom.Num(1),
+                        new Term.Atom.Variable("Y")));
+        final Term result2 = Parser.parse(string2);
+        assert (result2 instanceof Term.Compound);
+        assert (term2.equals(result2));
+    }
+
+    /**
+     * This test checks that the parser parses correctly compounds, ignoring spaces.
+     */
+    @Test
+    public void testSpacingCompounds() {
+        final Term correct_result = new Term.Compound("test",
+                new Term.Atom.Constant("a"),
+                new Term.Atom.Constant("b"),
+                new Term.Atom.Constant("c"));
+
+        // checking "test(a,b,c)" :
+        final String string1 = "test(a,b,c)";
+        final Term result1 = Parser.parse(string1);
+        assert (result1 instanceof Term.Compound);
+        assert (correct_result.equals(result1));
+
+        // checking "test( a,b,c )" :
+        final String string2 = "test( a,b,c )";
+        final Term result2 = Parser.parse(string2);
+        assert (result2 instanceof Term.Compound);
+        assert (correct_result.equals(result2));
+
+        // checking "test(a , b , c)" :
+        final String string3 = "test(a , b , c)";
+        final Term result3 = Parser.parse(string3);
+        assert (result3 instanceof Term.Compound);
+        assert (correct_result.equals(result3));
+
+        // checking "test( a , b , c )" :
+        final String string4 = "test( a , b , c )";
+        final Term result4 = Parser.parse(string4);
+        assert (result4 instanceof Term.Compound);
+        assert (correct_result.equals(result4));
     }
 }
